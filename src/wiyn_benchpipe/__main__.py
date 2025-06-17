697204996d4a641da5e535be4e75841b8cdd80eb
Ralf Kotulla <ralf.kotulla@gmail.com>
2025-06-17 17:18:21 -0500

    some updates to standalone code

---
+++ src/wiyn_benchpipe/__main__.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+from .executables import *
+
+args = sys.argv[1:]
+try:
+    command = args[0]
+except:
+    print("Unknown error")
+    sys.exit(-1)
+
+if (command == "reduce"):
+    wiyn_benchpipe(args[1:])
+else:
+    print("Unknown command %s" % (command))
+
