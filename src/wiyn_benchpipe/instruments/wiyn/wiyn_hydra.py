7e4317b79ad26536e54afd7b3efd2c469775f9a5
Ralf Kotulla <ralf.kotulla@gmail.com>
2025-08-05 16:55:22 -0500

    fixed some bugs with new hydra class

--- src/wiyn_benchpipe/instruments/wiyn/wiyn_hydra.py
+++ src/wiyn_benchpipe/instruments/wiyn/wiyn_hydra.py
@@ -2,7 +2,7 @@ import numpy
 import astropy.coordinates as coord
 import astropy.units as u
 
-from ..fibertraces import GenericFiberSpecs
+from ...fibertraces import GenericFiberSpecs
 from .wiyn_bench import wiyn_grating_from_header, WIYNBenchFiberSpecs
 
 class WiynHydraRedFiberSpecs( WIYNBenchFiberSpecs ):
@@ -11,7 +11,31 @@ class WiynHydraRedFiberSpecs( WIYNBenchFiberSpecs ):
     ref_fiber_id = 41
 
 class WiynHydraBlueFiberSpecs( WIYNBenchFiberSpecs ):
-    n_fibers = 90
+    n_fibers = 87
     name = "Hydra Blue Cable @ WIYN"
     ref_fiber_id = 45
 
+    trace_minx = 160
+    trace_maxx = 2520
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if (self.header is not None):
+            _n_fibers = 0
+            for i in range(1000):
+                slfib_key = "SLFIB%d" % (i+1)
+                if (slfib_key not in self.header):
+                    break
+                slfib = self.header[slfib_key]
+                if (slfib.strip() == ""):
+                    break
+                if (slfib.lower().find("broken fiber") > 0):
+                    self.logger.debug("Marking fiber %d as broken", i+1)
+                    continue
+                items = slfib.split()
+                fiber_type = int(items[1])
+                # if (fiber_type >= 0):
+                _n_fibers += 1
+            #self.n_fibers = _n_fibers
+            self.logger.info("Updating #fibers from header: %d" % self.n_fibers)
